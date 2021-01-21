8db2f5b95d5ef57b0da5f3345e04d35167909587
jorgetovar <jorgetovar621@gmail.com>
2021-01-20 20:45:03 -0500

    add functional implementation to current patterns

--- clojure-noob/src/clojure_noob/core.clj
+++ clojure-noob/src/clojure_noob/core.clj
@@ -1,9 +1,20 @@
 (ns clojure-noob.core
     (:gen-class))
 
+(defn my-loop []
+      (loop [x 15]
+            (when (> x 1)
+                  (println x)
+                  (recur (- x 2))))
+      )
 
 
 (defn -main
       "I don't do a whole lot ... yet."
       [& args]
-      (println "Hello, World!"))
+      (println "Enter something> ")
+      (let [x (read-line)]
+           (println (str "You typed " x))
+           )
+      (my-loop)
+      )
